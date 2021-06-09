33bc777f61c0037a3cc92cfe99c4cc24aca239e6
bruno <brlo.oliveira@gmail.com>
2021-06-09 20:57:31 -0300

    mvp 2 criada

--- app/src/main/java/com/devventure/lanceosdados/MainActivity.kt
+++ app/src/main/java/com/devventure/lanceosdados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devventure.lanceosdados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,17 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dado1 = findViewById<TextView>(R.id.dado_1)
-        val dado2 = findViewById<TextView>(R.id.dado_2)
-        val botao = findViewById<Button>(R.id.botao_gerar)
+        val dado1 = findViewById<ImageView>(R.id.dado_1)
+        val dado2 = findViewById<ImageView>(R.id.dado_2)
+        val btn = findViewById<Button>(R.id.botao_gerar)
 
-        botao.setOnClickListener{
-        dado1.text = gerarNumero().toString()
-        dado2.text = gerarNumero().toString()
+        val dices = listOf(R.drawable.dice_1, R.drawable.dice_2, R.drawable.dice_3, R.drawable.dice_4, R.drawable.dice_5, R.drawable.dice_6)
+
+        btn.setOnClickListener{
+            dado1.setImageResource(dices.random())
+            dado2.setImageResource(dices.random())
+
         }
     }
 
-    private fun gerarNumero() : Int {
-        return (1..6).random()
-    }
 }
